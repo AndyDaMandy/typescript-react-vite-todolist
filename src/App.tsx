e9536dcdc6e397c7945c3a3e04f9ad9b86a08d29
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-06-09 15:07:32 -0400

    set up Todo interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
 import InputField from "./components/InputField.tsx";
+import {Todo} from "./components/model.ts";
 
 const App: React.FC = () => {
     const [todo, setTodo] = useState<string>("");
+    const [todos, setTodos] = useState<Todo[]>([]);
+    console.log(todo);
   return (
     <div className="App">
         <h1 className="heading">Taskify</h1>
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './styles.css';
 
+//this sets up the types for the props. The React.Dispatch comes from the array destrucruting type.
 interface Props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
 }
-const InputField = ({todo, setTodo}: Props) => {
+
+//tldr if you're passing props from a parent, you need the inputfield
+const InputField: React.FC<Props> = ({todo, setTodo}) => {
     return (
         <form className="input">
-            <input type="input" placeholder="Enter" className="input_box"></input>
+            <input type="input"
+                   value={todo}
+                   onChange={
+                    //e stands for events. What we're doing here is setting the target value.
+                       (e) => setTodo(e.target.value)
+                   }
+                   placeholder="Enter" className="input_box"></input>
             <button className="input_submit" type="submit">Go</button>
         </form>
     )
