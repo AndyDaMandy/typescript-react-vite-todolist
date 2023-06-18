6955362fe40ba94eb7c3c3615e25314438d425b6
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-06-17 20:36:49 -0400

    added styling and set up handleDone for SingleTodo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Todo} from "./model.ts";
+import {AiFillDelete, AiFillEdit} from 'react-icons/ai';
+import {MdDone} from "react-icons/md";
 
 //what's the difference between a type and a
 type Props = {
@@ -9,9 +11,28 @@ type Props = {
 }
 
 const SingleTodo = ({todo, todos, setTodos}: Props) => {
+    const handleDone = (id: number) => {
+        setTodos(
+            //maps through the array and if the ID matches sets isDone to opposite
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+            )
+        )
+    };
     return (
         <form className="todos__single">
             <span className="todos__single--text">{todo.todo}</span>
+            <div>
+                <span className="icon">
+                    <AiFillEdit />
+                </span>
+                <span className="icon">
+                    <AiFillDelete />
+                </span>
+                <span className="icon" onClick={() => handleDone(todo.id)}>
+                    <MdDone />
+                </span>
+            </div>
         </form>
     );
 };
